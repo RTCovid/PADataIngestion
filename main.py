6ba3873ffa4be8b392c310c591e1336dcc745a79
Adam Fletcher <adamfblahblah@gmail.com>
2020-03-30 20:34:51 -0700

    Fix bug with ArcGIS CSV parser not handling apostrophes in the header names

--- main.py
+++ main.py
@@ -27,12 +27,13 @@ def load_credentials():
         
 
 # On Google Cloud, only /tmp is writeable.
-def get_latest_file(creds, prefix="HOS_ResourceCapacity", target_dir="/tmp/"):
+def get_latest_file(creds, prefix="HOS_ResourceCapacity", target_dir="/tmp"):
     cnopts = pysftp.CnOpts()
     cnopts.hostkeys.load('copaftp.pub')
     username = creds['sftp']['username']
     password = creds['sftp']['password']
     host = creds['sftp']['host']
+    latest_filename = ""
     with pysftp.Connection(host, username=username, password=password, cnopts=cnopts) as sftp:
         files = sftp.listdir()
         files = [f for f in files if f.startswith("HOS_ResourceCapacity")]
@@ -40,10 +41,36 @@ def get_latest_file(creds, prefix="HOS_ResourceCapacity", target_dir="/tmp/"):
         # Filenames look like HOS_ResourceCapacity_2020-03-30_00-00.csv
         # And timestamps are in UTC
         latest_filename = files[-1]
-        print("The latest file is: f{latest_filename}")
+        print(f"The latest file is: {latest_filename}")
         sftp.get(latest_filename, f'{target_dir}/{latest_filename}')
-        print("Finished downloading f{latest_filename}")
-        return (latest_filename, target_dir)
+        print(f"Finished downloading {target_dir}/{latest_filename}")
+    return (target_dir, latest_filename)
+
+
+def process_csv(source_data_dir, source_data_file, tmpdir="/tmp"):
+   # data = pd.read_csv(os.path.join(source_data_dir, source_data_file), engine="python")
+    output_filename = "processed_HOS.csv"
+    output_dir = tmpdir
+    output_path = os.path.join(output_dir, output_filename)
+    rows = []
+    with open (os.path.join(source_data_dir, source_data_file), newline='') as rf:
+        reader = csv.reader(rf)
+        header = True
+        for row in reader:
+            if header:
+                header_row = []
+                for c in row:
+                    if "'" in c:
+                        c = c.replace("'", "")
+                    header_row.append(c)
+                rows.append(header_row)
+                header = False
+            else:
+                rows.append(row)
+    with open (output_path, 'w', newline='') as wf:
+        writer = csv.writer(wf)
+        writer.writerows(rows)
+    return (output_dir, output_filename)
 
 def upload_to_arcgis(creds, source_data_dir, source_data_file, original_data_file_name, 
                     arcgis_item_id_for_feature_layer):
@@ -80,23 +107,27 @@ def upload_to_arcgis(creds, source_data_dir, source_data_file, original_data_fil
         os.chdir(tmpdirname)
         result = fs.manager.overwrite(original_data_file_name)
         os.chdir(original_dir)
-        os.remove(os.path.join(source_data_dir, source_data_file))
+        #os.remove(os.path.join(source_data_dir, source_data_file))
     return result
 
 
 def main():
+    print("Started ingestion processing run")
     # The name of the file you created the layer service with.
-    original_data_file_name = "HOS_ResourceCapacity_2020-03-28_21-00.csv"
+    original_data_file_name = "processed_HOS.csv"
     # You can get this id from the URL in arcgis online when you look at layer; eg:
     # https://pema.maps.arcgis.com/home/item.html?id=b815071a19394023872f5dd88f273614
-    arcgis_item_id_for_feature_layer = "b815071a19394023872f5dd88f273614"
+    # This would be the page with Source: Feature Service on it.
+    arcgis_item_id_for_feature_layer = "38592574c8de4a02b180d6f65918e385"
 
     creds = load_credentials()
-    latest_filename, data_dir= get_latest_file(creds)
-    print(data_dir, latest_filename)
-    status = upload_to_arcgis(creds, data_dir, latest_filename, 
+    data_dir, latest_filename = get_latest_file(creds)
+    processed_dir, processed_filename = process_csv(data_dir, latest_filename)
+    print(f"Finished processing {data_dir}/{latest_filename}, file is {processed_dir}/{processed_filename}")
+    status = upload_to_arcgis(creds, processed_dir, processed_filename, 
                             original_data_file_name, arcgis_item_id_for_feature_layer)
-    print(latest_filename, status)
+    print(status)
+    print("Finished ingestion processing run")
 
 
 def hello_pubsub(event, context):
