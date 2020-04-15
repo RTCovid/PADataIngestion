f3930712c1e944f87726e375194532d2fa928db1
Adam Fletcher <adamfblahblah@gmail.com>
2020-04-14 22:04:19 -0700

    Move to using two cloud functions

--- deploy_to_gcloud.sh
+++ deploy_to_gcloud.sh
@@ -1,4 +1,4 @@
-SETTINGS="--runtime python37 --memory=256MB --timeout=180s"
+SETTINGS="--runtime python37 --memory=1024MB --timeout=180s"
 #gcloud functions deploy  bitdotioinc-pa-covid-data-ingest 
 gcloud functions deploy  bitdotioinc-pa-covid-data-ingest-instantaneous ${SETTINGS} --entry-point=instantaneous_pubsub --trigger-topic=trigger-pa-data-ingestion-instantaneous-cloud-function
 gcloud functions deploy  bitdotioinc-pa-covid-data-ingest-historical ${SETTINGS} --entry-point=historical_pubsub --trigger-topic=trigger-pa-data-ingestion-historical-cloud-function
